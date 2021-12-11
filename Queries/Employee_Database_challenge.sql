05c1e14849ef688b189944f54d651f123775986a
Trevor Harren <trevor.harren@gmail.com>
2021-12-11 17:21:04 -0500

    Module 7 Assignment upload.

---
+++ Queries/Employee_Database_challenge.sql
@@ -0,0 +1,60 @@
+--Create table with all soon to retire employees with all previous job titles
+
+SELECT  e.emp_no,
+		e.first_name,
+		e.last_name,
+		t.title,
+		t.from_date,
+		t.to_date
+INTO ret_titles
+FROM employees as e
+INNER JOIN titles as t
+ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no;
+
+
+-- Use Dictinct with Orderby to remove duplicate rows
+
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+		e.first_name,
+		e.last_name,
+		t.title
+INTO ret_unq_title
+FROM employees as e
+INNER JOIN titles as t
+ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no, t.to_date DESC;
+
+
+-- Create table with counts of retirees by Title
+
+SELECT COUNT (ut.emp_no),
+		ut.title
+INTO ret_title_cnt
+FROM ret_unq_title AS ut
+GROUP BY ut.title;
+
+
+-- Create Mentorship-Eligible table
+
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+		e.first_name,
+		e.last_name,
+		e.birth_date,
+		de.from_date,
+		de.to_date,
+		t.title
+INTO ment_elig
+FROM employees as e
+	INNER JOIN dept_emp as de
+	ON (e.emp_no = de.emp_no)
+	INNER JOIN titles as t
+	ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+AND de.to_date = ('9999-01-01')
+ORDER BY e.emp_no;
+
+
+
